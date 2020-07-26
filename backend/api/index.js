c0d7b70eb57d0abdb185138cc5fdd5d0603acdd2
japnoor <jp238552@dal.ca>
2020-07-25 23:08:18 -0300

    Team Management Feature

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,20 +1,21 @@
-const express = require('express');
+const express = require("express");
 const apiRoutes = express();
 
-const TaskRoutes = require('../modules/task/routes')
-const ContactUsRoutes = require('../modules/contact-us/routes')
-const RegisterRoutes = require('../modules/register/routes')
-const ProjectRoutes = require('../modules/project/routes')
-const LoginRoutes = require('../modules/login/routes')
-const ResetPasswordRoutes = require('../modules/reset-password/routes')
+const TaskRoutes = require("../modules/task/routes");
+const ContactUsRoutes = require("../modules/contact-us/routes");
+const RegisterRoutes = require("../modules/register/routes");
+const ProjectRoutes = require("../modules/project/routes");
+const LoginRoutes = require("../modules/login/routes");
+const ResetPasswordRoutes = require("../modules/reset-password/routes");
+const TeamRoutes = require("../modules/team/routes");
 
 // base url for each module
-apiRoutes.use('/task', TaskRoutes)
-apiRoutes.use('/contact-us', ContactUsRoutes);
-apiRoutes.use('/register', RegisterRoutes);
-apiRoutes.use('/project', ProjectRoutes)
-apiRoutes.use('/login', LoginRoutes);
-apiRoutes.use('/resetpassword', ResetPasswordRoutes)
-
+apiRoutes.use("/task", TaskRoutes);
+apiRoutes.use("/contact-us", ContactUsRoutes);
+apiRoutes.use("/register", RegisterRoutes);
+apiRoutes.use("/project", ProjectRoutes);
+apiRoutes.use("/login", LoginRoutes);
+apiRoutes.use("/resetpassword", ResetPasswordRoutes);
+apiRoutes.use("/teams", TeamRoutes);
 
 module.exports = apiRoutes;
