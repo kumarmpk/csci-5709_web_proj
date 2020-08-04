91b3dc422ffb82e3faf2654e7dabe7c64b260e17
Falgun Dharmeshbhai Patel <fl889399@dal.ca>
2020-08-04 15:01:47 -0300

    sprint management front end

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -31,6 +31,7 @@ import DocumentsProject from "./documents/DocumentsProject";
 import Documents from "./documents/Documents";
 import NewDocument from "./documents/NewDocument";
 import DocumentUpdate from "./documents/DocumentUpdate";
+import SprintManagement from "./sprint-management/SprintManagement";
 
 class Routes extends Component {
   constructor(props) {
@@ -174,6 +175,7 @@ class Routes extends Component {
               path="/register"
               render={(props) => <Register {...props}></Register>}
             ></Route>
+            <Route path="/sprint" component={SprintManagement}></Route>
             <Route path="/about-us" component={AboutUs}></Route>
             <Route path="/contact-us" component={ContactUs}></Route>
 
