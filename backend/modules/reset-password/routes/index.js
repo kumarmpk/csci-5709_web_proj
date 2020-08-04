32d9252c914383eb293862616539a676e5b539d8
manoharan <pr971100@dal.ca>
2020-08-04 12:55:15 -0300

    master

--- backend/modules/reset-password/routes/index.js
+++ backend/modules/reset-password/routes/index.js
@@ -4,6 +4,8 @@
 const express = require("express");
 const reset_password_router = express.Router();
 const { ResetPasswordController } = require("../controllers/ResetPasswordController");
+const { OtpController } = require("../controllers/OtpController");
+const { VerifyOtpController } = require("../controllers/VerifyOtpController");
 
 
 reset_password_router.post("/", (req, res) => {
@@ -17,6 +19,44 @@ reset_password_router.post("/", (req, res) => {
                 return res.status(200).send("Password updated successfully!");
             else
                 return res.status(500).send("Password updated failed");
+        });
+    } catch (err) {
+        console.log("err", err);
+        return res.status(500).send("Password updation failed due to unknown causes");
+    }
+});
+
+reset_password_router.post("/otp", (req, res) => {
+    try {
+        let userData = req.body.data;
+        console.log("came to otp.js");
+        console.log(userData);
+
+        OtpController(userData, (response) => {
+            if (response === "38")
+                return res.status(200).send("Otp inserted successfully!");
+            else
+                return res.status(500).send("Otp insertion failed");
+        });
+    } catch (err) {
+        console.log("err", err);
+        return res.status(500).send("Password updation failed due to unknown causes");
+    }
+});
+
+reset_password_router.post("/verify", (req, res) => {
+    try {
+        let userData = req.body.data;
+        console.log("came to verify");
+        console.log(userData);
+
+        VerifyOtpController(userData, (response) => {
+            if (response === "41")
+                return res.status(200).send("Otp validation failed!");
+            else if (response === "42")
+                return res.status(400).send("OTP entered does not match with the sent OTP");
+            else
+                return res.status(200).send("Otp validated successfully");
         });
     } catch (err) {
         console.log("err", err);
