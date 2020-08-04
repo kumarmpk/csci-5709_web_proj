447f4fedd1daa572210a76e64c73c60f13d2c31f
Falgun Dharmeshbhai Patel <fl889399@dal.ca>
2020-08-04 14:12:09 -0300

    Updated Sprint MAnagement backend

--- frontend/src/constants.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  encryption_algorithm: "aes-128-cbc",
-  encryption_password: "protracker",
-  //URL: "http://localhost:4000/",
-  URL: "https://csci5709-group11-backend.herokuapp.com/",
-};
