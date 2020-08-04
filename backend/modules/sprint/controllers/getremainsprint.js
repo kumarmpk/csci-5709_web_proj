ff0754ee970a146ee088e1f87c6f2f062f2699ec
Falgun Dharmeshbhai Patel <fl889399@dal.ca>
2020-08-04 14:58:45 -0300

    Sprint Management Backend controllers

--- backend/modules/sprint/controllers/getremainsprint.js
+++ backend/modules/sprint/controllers/getremainsprint.js
@@ -5,23 +5,36 @@
 const connectionObject = require('../../../MySQLCon');
 
 var getNotCompletedSprint = (req, res) => {
+    // console.log(req.body);
     if (
-        !req.body.hasOwnProperty("projectID")
+        !req.body.hasOwnProperty("projectID") || req.body['projectID'] == null
     ) {
         return res
             .status(400)
             .json({ msg: `Please include all mandatory details- projectID` });
+    } else if (Number.isInteger(req.body['projectID']) == false) {
+        return res
+            .status(400)
+            .json({ msg: `Please enter valid projectID` });
     }
 
-    whereSprint = 'WHERE projectID = ? and isComplete is ? or isComplete = ?'
+    whereSprint = 'WHERE projectID = ? and (isComplete is ? or isComplete = ?)'
     let sprintStatus = 'SELECT * from Sprints ' + whereSprint
     let sprintID = [req.body.projectID, null, 0]
 
     connectionObject.query(sprintStatus, sprintID, (err, result) => {
+        // console.log("abcacbacbacbacb", result);
         if (err) {
             res.status(503).json({ msg: `error while fetching sprint details of project with projectID: ${req.body.projectID} from database` });
         } else {
-            console.log('');
+            // console.log('result', result[0]['isComplete'][0]);  // bija cases ma
+            // console.log('result', result[1]['isComplete']);   //null hoi tyare
+            // console.log('result', result);
+            result.forEach(element => {
+                if (element['isComplete'] != null) {
+                    element['isComplete'] = element['isComplete'][0]
+                }
+            });
             res.status(200).json({ msg: `successfully got sprint details for project with projectID: ${req.body.projectID}` 
         , data: JSON.parse(JSON.stringify(result))});
         }
--- backend/modules/sprint/controllers/modifytaskstatus.js
+++ backend/modules/sprint/controllers/modifytaskstatus.js
@@ -5,34 +5,47 @@
 const connectionObject = require('../../../MySQLCon')
 
 var modifyTaskStatus = (req, res) => {
+    // console.log('req',req.body);
     if (
         !req.body.hasOwnProperty("sprintID") ||
         !req.body.hasOwnProperty("taskID") ||
         !req.body.hasOwnProperty("projectID") ||
-        !req.body.hasOwnProperty("newStatus")
+        !req.body.hasOwnProperty("newStatus") || req.body['sprintID'] == null || req.body['taskID'] == null || req.body['projectID'] == null
         
     ) {
         return res
             .status(400)
             .json({ msg: `Please include all mandatory details- sprintID, taskID, projectID, and newStatus` });
+    } else if ((typeof req.body['newStatus'] == "string") || Number.isInteger(req.body['sprintID']) == false || Number.isInteger(req.body['taskID']) == false || Number.isInteger(req.body['projectID']) == false) {
+        return res
+            .status(400)
+            .json({ msg: `Please enter valid newStatus, sprintID, taskID, and projectID` });
     }
+
 
     whereSprint = 'WHERE sprintID = ? and projectID = ?'
     let sprintStatus = 'SELECT Sprints.isComplete as status from Sprints ' + whereSprint
-    let sprintID = [req.body.newSprintID, req.body.projectID]
+    let sprintID = [req.body.sprintID, req.body.projectID]
 
-    let where = 'taskid = ? and projectid = ? and sprintid = ?';
+    let where = 'WHERE taskid = ? and projectid = ? and sprintid = ?';
     let updateTaskStatus = 'UPDATE Tasks SET Tasks.status = ? ' + where;
-    let update_task_detail = [req.body.newStatus, req.body.taskID, req.body.projectID, req.body.SprintID];
+    let update_task_detail = [req.body.newStatus, req.body.taskID, req.body.projectID, req.body.sprintID];
 
     connectionObject.query(sprintStatus, sprintID, (err, result) => {
         if (err) {
             res.status(503).json({ msg: `error while fetching status of sprint sprintID: ${req.body.sprintID}  from database` });
         } else {
+            // console.log(result);
+            result.forEach(element => {
+                if (element['status'] != null) {
+                    element['status'] = element['status'][0]
+                }
+            });
             if (result[0].status == 0) {
                 connectionObject.query(updateTaskStatus, update_task_detail, (err, result) => {
+                    // console.log('res', result.message);
                     if (err) {
-                        res.status(503).json({ msg: `error while updating sprint of task taskID: ${req.body.taskID}  from database` });
+                        res.status(503).json({ msg: `error while updating sprint of task with taskID: ${req.body.taskID}  from database` });
                     } else {
                         res.status(200).json({ msg: `status of task with taskID: ${req.body.taskID} successfully updated in database` });
                     }
